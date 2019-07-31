fcbfeea009ad5a8ede4d881e3bb97a6e2f8427eb
An0ncoder <platodroid@gmail.com>
2019-07-31 13:19:55 +0200

    use promises

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,21 +1,56 @@
 <template>
-    <div class="single-post-page">
-        <section class="post">
-            <h1 class="post-title">Title of the post</h1>
-            <div class="post-details">
-                <div class="post-detail">Last updated on XXX</div>
-                <div class="post-detail">Written by NAME</div>
-            </div>
-            <p class="post-content">Content of the post</p>
-        </section>
-        <section class="post-feedback">
-            Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domainzz.com">feedback@my-awesome-domainzz.com</a>
-        </section>
-    </div>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
+      <div class="post-details">
+        <div class="post-detail">{{ loadedPost.updatedDate }}</div>
+        <div class="post-detail">{{ loadedPost.author }}</div>
+      </div>
+      <p class="post-content">{{ loadedPost.content }}</p>
+    </section>
+    <section class="post-feedback">
+      Let me know what you think about the post, send a mail to
+      <a
+        href="mailto:feedback@my-awesome-domainzz.com"
+      >feedback@my-awesome-domainzz.com</a>
+    </section>
+  </div>
 </template>
 
+<script>
+export default {
+  asyncData(context, callback) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPost: {
+            id: "1",
+            title: "First post (ID: " + context.params.id + ")",
+            previewText: "This is our first post",
+            author: "Anon",
+            updatedDate: new Date(),
+            content:
+              "Some dummy text which is definitely not the preview text.",
+            thumbnail:
+              "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_960_720.jpg",
+            text: "just some text"
+          }
+        });
+      }, 1000);
+    })
+    then((data) => {
+
+    })
+    .catch((e) => {
+      context.error(new Error());
+    })
+  }
+};
+</script>
+
+
 <style scoped>
-    .single-post-page {
+.single-post-page {
   padding: 30px;
   text-align: center;
   box-sizing: border-box;
